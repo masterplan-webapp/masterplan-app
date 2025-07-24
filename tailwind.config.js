3ce6f03ae5544d641090b7d214d418e44d0f8cca
masterplan-webapp <masterplan.webapp@gmail.com>
2025-07-24 01:53:29 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   darkMode: 'class',
   content: [
     "./index.html",
-    "./*.{js,ts,jsx,tsx}",
-    "./components.tsx",
-    "./App.tsx"
+    "./**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
